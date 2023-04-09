44bc0ccca5e3a5bf3c67a5c59a0c419f50e64634
tometo8284 <129486534+tometo8284@users.noreply.github.com>
2023-04-09 05:56:29 +0000

    22章まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   get 'homes/about', as: 'about'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
